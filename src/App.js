ea806f57ea8346d3e30b27f3d57951c334f59210
cdenny17 <cdenny83@gmail.com>
2018-05-06 22:09:35 -0700

    testing git hub commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return ( <
-            div className = "App" >
-            <
-            div className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h2 > Welcome to React < /h2> <
-            /div> <
-            p className = "App-intro" >
-            blahblahblahbalh <
-            /p> <
-            /div>
+        return (
+            <p>Hello</p>
         );
     }
 }
